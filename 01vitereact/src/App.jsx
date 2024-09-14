9b75c92ea1cbb810ae83f2eaeea79661786354cc
SamratThapa99 <126045530+SamratThapa99@users.noreply.github.com>
2024-09-14 13:01:35 +0545

    created project using create react project & vite ,

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+  const username= "samrat thapa"
+  
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test</p>
+    </>
+  )
+}
+
+export default App
