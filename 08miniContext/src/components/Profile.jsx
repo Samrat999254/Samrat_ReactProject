d850482a65be115b674c866bb7cfc9ac3d9f3f31
SamratThapa99 <126045530+SamratThapa99@users.noreply.github.com>
2024-09-20 13:10:43 +0545

    context api discussion and simple project

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+// Data Receiving
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+    const {user} = useContext(UserContext)
+
+    if (!user) {
+        return(
+        <div>Please login</div>
+        )
+    }
+
+    return <div>Welcome {user.username}/</div>
+ 
+}
+
+export default Profile
